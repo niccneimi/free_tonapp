a5ae0c047dfa042feecb67c88789896d68302e8f
plaschinskij nikolaj <niccneimi@gmail.com>
2025-05-31 17:16:28 +0300

    feat: значок тона в таблице выплат

--- src/components/RollTable.jsx
+++ src/components/RollTable.jsx
@@ -1,5 +1,5 @@
 import './RollTable.css';
-
+import tonIcon from '../assets/small_ton.svg';
 export default function RollTable() {
     return (
         <>
@@ -10,27 +10,27 @@ export default function RollTable() {
             <div className="roll-table">
                 <div className="roll-table-item">
                     <span className="roll-table-item-number">0 - 9885</span>
-                    <span className="roll-table-item-payment">0.000092</span>
+                    <span className="roll-table-item-payment">0.000092<img src={tonIcon} alt="TON" className="payment-icon" /></span>
                 </div>
                 <div className="roll-table-item">
                     <span className="roll-table-item-number">9886 - 9985</span>
-                    <span className="roll-table-item-payment">0.000667</span>
+                    <span className="roll-table-item-payment">0.000667<img src={tonIcon} alt="TON" className="payment-icon" /></span>
                 </div>
                 <div className="roll-table-item">
                     <span className="roll-table-item-number">9986 - 9993</span>
-                    <span className="roll-table-item-payment">0.003667</span>
+                    <span className="roll-table-item-payment">0.003667<img src={tonIcon} alt="TON" className="payment-icon" /></span>
                 </div>
                 <div className="roll-table-item">
                     <span className="roll-table-item-number">9994 - 9997</span>
-                    <span className="roll-table-item-payment">0.367769</span>
+                    <span className="roll-table-item-payment">0.367769<img src={tonIcon} alt="TON" className="payment-icon" /></span>
                 </div>
                 <div className="roll-table-item">
                     <span className="roll-table-item-number">9998 - 9999</span>
-                    <span className="roll-table-item-payment">3.67</span>
+                    <span className="roll-table-item-payment">3.67<img src={tonIcon} alt="TON" className="payment-icon" /></span>
                 </div>
                 <div className="roll-table-item">
                     <span className="roll-table-item-number">10000</span>
-                    <span className="roll-table-item-payment">10</span>
+                    <span className="roll-table-item-payment">10<img src={tonIcon} alt="TON" className="payment-icon" /></span>
                 </div>
                 <div className="line"><div className="line-item"/></div>                    
             </div>
